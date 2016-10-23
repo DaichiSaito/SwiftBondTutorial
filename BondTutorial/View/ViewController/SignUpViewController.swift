53ae79991665345c6ca8688e15c12109442a1c58
DaichiSaito <h1a_ble@yahoo.co.jp>
2016-10-23 12:48:55 +0900

    Initial Commit

---
+++ BondTutorial/View/ViewController/SignUpViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SignUpViewController.swift
+//  BondTutorial
+//
+//  Created by Takahiro Nishinobu on 2016/01/31.
+//  Copyright © 2016年 hachinobu. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var warningLabel: UILabel!
+    @IBOutlet weak var loginIDTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmationTextField: UITextField!
+    @IBOutlet weak var agreementSwitch: UISwitch!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    var signUpVM = SignUpVM()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupBind()
+    }
+
+    private func setupBind() {
+        // VMのloginIDをViewのTextFieldに紐付けている。bnd_text等はお決まりの書き方。
+        signUpVM.loginID.bidirectionalBindTo(loginIDTextField.bnd_text)
+        signUpVM.password.bidirectionalBindTo(passwordTextField.bnd_text)
+        signUpVM.passwordConfirmation.bidirectionalBindTo(passwordConfirmationTextField.bnd_text)
+        signUpVM.isAgreement.bidirectionalBindTo(agreementSwitch.bnd_on)
+        
+        signUpVM.signUpViewStateInfo.observe { [weak self] (buttonEnabled, buttonAlpha, warningMessage) -> Void in
+            self?.warningLabel.text = warningMessage
+            self?.signUpButton.enabled = buttonEnabled
+            self?.signUpButton.alpha = buttonAlpha
+        }
+        
+        signUpVM.isLoadingViewHidden.bindTo(loadingIndicator.bnd_hidden)
+        signUpVM.isLoadingViewAnimate.bindTo(loadingIndicator.bnd_animating)
+        
+        // 新規登録ボタンがタップされるたびにSignUpVMクラスのsignUpメソッドを実行するという意味です
+        signUpButton.bnd_controlEvent.filter { $0 == .TouchUpInside }.observe { [weak self] _ -> Void in
+            self?.signUpVM.signUp()
+        }
+        
+        signUpVM.finishSignUp.ignoreNil().observe { [weak self] (email, password) -> Void in
+            let alertController = UIAlertController(title: "メッセージ", message: "loginID:\(email)\npassword:\(password)", preferredStyle: .Alert)
+            let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            alertController.addAction(action)
+            self?.navigationController?.presentViewController(alertController, animated: true, completion: nil)
+        }
+        
+    }
+    
+}
---
+++ BondTutorial/ViewModel/SignUpVM.swift
@@ -0,0 +1,91 @@
+//
+//  SignUpVM.swift
+//  BondTutorial
+//
+//  Created by Takahiro Nishinobu on 2016/01/31.
+//  Copyright © 2016年 hachinobu. All rights reserved.
+//
+
+import Foundation
+import Bond
+
+class SignUpVM {
+    
+    enum RequestState {
+        case None
+        case Requesting
+        case Finish
+    }
+    
+    let requestState = Observable<RequestState>(.None)
+    let loginID = Observable<String?>("")
+    let password = Observable<String?>("")
+    let passwordConfirmation = Observable<String?>("")
+    let isAgreement = Observable<Bool>(false)
+    
+    var isLoadingViewAnimate: EventProducer<Bool> {
+        //$0は変化後のrequestStateの値
+        return requestState.map { $0 == .Requesting }
+    }
+    
+    var isLoadingViewHidden: EventProducer<Bool> {
+        //$0は変化後のrequestStateの値
+        return
+            requestState.map { $0 != .Requesting }
+    }
+    
+    var finishSignUp: EventProducer<(email: String, password: String)?> {
+        
+        return requestState.map { [weak self] reqState in
+            
+            guard let email = self?.loginID.value, password = self?.password.value where reqState == .Finish else {
+                return nil
+            }
+            
+            return (email, password)
+        
+        }
+        
+    }
+    
+    var signUpViewStateInfo: EventProducer<(buttonEnabled: Bool, buttonAlpha: CGFloat, warningMessage: String)> {
+        
+        return combineLatest(requestState, loginID, password, passwordConfirmation, isAgreement).map { (reqState, loginID, password, passwordConfirmation, isAgreement) in
+            
+            //通信中の場合
+            guard let loginID = loginID, password = password, passwordConfirmation = passwordConfirmation where reqState != .Requesting else {
+                // ここの引数がそれぞれbuttonEnabled,buttonAlpha,warningMessageに対応している。
+                return (false, 0.5, "")
+            }
+            //3つのTextFieldがすべて埋まっていない場合
+            if loginID.isEmpty || password.isEmpty || passwordConfirmation.isEmpty {
+                return (false, 0.5, "")
+            }
+            
+            guard password == passwordConfirmation else {
+                return (false, 0.5, "パスワードと確認パスワードが一致していません")
+            }
+            
+            guard isAgreement else {
+                return (false, 0.5, "規約に同意してください")
+            }
+            
+            return (true, 1.0, "")
+            
+        }
+        
+    }
+    
+    func signUp() {
+        
+        requestState.next(.Requesting)
+        let delay = 1.0 * Double(NSEC_PER_SEC)
+        let time  = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        
+        dispatch_after(time, dispatch_get_main_queue()) { [unowned self] in
+            self.requestState.next(.Finish)
+        }
+        
+    }
+    
+}
